3d16685dae13f58d7f852e9e9723f21c088af29e
hallowlab <oscarcastillo00824@gmail.com>
2024-04-13 09:16:45 -0400

    update

---
+++ src/components/Menu/SocialIcons/SocialIcons.constants.jsx
@@ -0,0 +1,7 @@
+// import cv from "./CV.pdf";
+
+export const socialLinks = [
+  { name: "Github", link: "https://github.com/hallowlab" },
+  // {name: "Resume", link: cv},
+  { name: "Mail", link: "mailto:oscarcastillo00824@gmail.com" },
+];
